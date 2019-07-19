4e6f5ef8ff5cdbf1484d75f7e189c4bcad7790a7
Matthew Grainger <matthew.grainger@nina.no>
2019-07-19 15:17:16 +0200

    Build datasets outside of the app

---
+++ Data_save.R
@@ -0,0 +1,41 @@
+GS_id<-"aZ4E5I4AAAAJ&hl"
+profile<-get_profile(GS_id)
+cite.yr<-get_citation_history(GS_id)
+pubs<-get_publications(GS_id)
+predH<-predict_h_index(GS_id)
+saveRDS(profile, "Profile.RDS")
+saveRDS(cite.yr, "citeyr.RDS")
+saveRDS(pubs, "pubs.RDS")
+saveRDS(predH, "predH.RDS")
+# Define the folder where the text files are
+a <-Corpus(VectorSource(WoSpapers$AB))
+a
+# Preprocessing text
+a <- tm_map(a, removeNumbers) # Not necessary if numbers are important for you
+a <- tm_map(a, removePunctuation)
+a <- tm_map(a , stripWhitespace)
+a <- tm_map(a, tolower)
+# Stopwords are words such as "we" "the" "and" "so", etc. You can add your own words to the list
+a <- tm_map(a, removeWords, c(stopwords("english"), "can", "also", "may"))
+# a <- tm_map(a, stemDocument, language = "english") # You can also do steamming if you want
+
+# Computing the term document matrix
+tdm <- TermDocumentMatrix(a)
+
+# Transforming data for wordcloud
+m <- as.matrix(tdm)
+v <- sort(rowSums(m), decreasing=TRUE)
+myNames <- names(v)
+d <- data.frame(word=myNames, freq=v)
+saveRDS(d, "d.RDS")
+
+timeline_dat <- data.frame(
+  id      = 1:4,
+  content = c("University of Pretoria","World Pheasant Association", "Newcastle University",
+              "Norwegian Institute for Nature Research"),
+  start   = c("2009-01-01","2011-04-01", "2012-12-01",
+              "2019-01-04"),
+  end     = c("2012-01-01","2012-11-01", "2018-12-31",NA)
+)
+
+saveRDS(timeline_dat,"timeline_dat.RDS")
--- app.R
+++ app.R
@@ -6,6 +6,7 @@ library(scholar)
 library(timevis)
 library(tm)
 library(wordcloud)
+library(rsconnect)
 ###########################
 
 #Build shinyapp
@@ -90,59 +91,26 @@ ui <- dashboardPage(
 server <- function(input, output) { 
   
   #some data manipulation to derive the values of boxes
-  GS_id<-"aZ4E5I4AAAAJ&hl"
-  profile<-get_profile(GS_id)
-  cite.yr<-get_citation_history(GS_id)
-  pubs<-get_publications(GS_id)
-  
-  
-  
-  pubstab<-pubs %>% 
+profile<-readRDS("Profile.RDS")
+pubs<-readRDS("pubs.RDS")
+predH<-readRDS("predH.RDS")
+cite.yr<-readRDS("citeyr.RDS")
+
+d<-readRDS("d.RDS")
+timeline_dat<-readRDS("timeline_dat.RDS")
+pubstab<-pubs %>% 
     select(title, journal, number, year) %>% 
     mutate(year=round(year)) %>% 
     rename("Title"=title, "Journal"=journal, "Journal & Page numbers"=number, "Year"=year)
   
-  #reactive data
-  sortTable <- reactive({
+#reactive data
+sortTable <- reactive({
     pubstab[do.call(order, pubstab[as.character(input$sort_on)]),]
   })
   
-  predH<-predict_h_index(GS_id)
+  
   
   WoSpapers <- read_delim("WoSpapers.csv", ";", escape_double = FALSE, trim_ws = TRUE)
-  
-  
-  # Define the folder where the text files are
-  a <-Corpus(VectorSource(WoSpapers$AB))
-  a
-  # Preprocessing text
-  a <- tm_map(a, removeNumbers) # Not necessary if numbers are important for you
-  a <- tm_map(a, removePunctuation)
-  a <- tm_map(a , stripWhitespace)
-  a <- tm_map(a, tolower)
-  # Stopwords are words such as "we" "the" "and" "so", etc. You can add your own words to the list
-  a <- tm_map(a, removeWords, c(stopwords("english"), "can", "also", "may"))
-  # a <- tm_map(a, stemDocument, language = "english") # You can also do steamming if you want
-  
-  # Computing the term document matrix
-  tdm <- TermDocumentMatrix(a)
-  
-  # Transforming data for wordcloud
-  m <- as.matrix(tdm)
-  v <- sort(rowSums(m), decreasing=TRUE)
-  myNames <- names(v)
-  d <- data.frame(word=myNames, freq=v)
-  # Making and displaying the cloud
-  #wordcloud(d$word, d$freq, min.freq=5,colors=brewer.pal(8, "Dark2"))
-  
-  timeline_dat <- data.frame(
-    id      = 1:4,
-    content = c("University of Pretoria","World Pheasant Association", "Newcastle University",
-                "Norwegian Institute for Nature Research"),
-    start   = c("2009-01-01","2011-04-01", "2012-12-01",
-                "2019-01-04"),
-    end     = c("2012-01-01","2012-11-01", "2018-12-31",NA)
-  )
   
   
   
